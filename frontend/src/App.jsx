29306e94d69fe38692595e91a20d2ea792eb5ebd
wnicolas <nicolas.buitrago2gmail.com>
2021-07-14 21:57:17 -0500

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 // import { Link, Route, Switch } from "react-router-dom";
-import { Login } from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      
     </div>
   );
 }
