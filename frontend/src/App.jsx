f8d9c7f66d1dba0fe8e5710bf899cc00b8f713a6
wnicolas <nicolas.buitrago2gmail.com>
2021-07-14 21:54:57 -0500

    Proyecto fresco

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css';
-import { Link, Route, Switch } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+// import { Link, Route, Switch } from "react-router-dom";
 import { Login } from './components/Login';
 
 function App() {
