146e128f981aabacb37d4e723a0eb91e04924a80
Camilost1404 <camitorres1404@gmail.com>
2021-07-15 00:30:54 -0500

    Llamando componente Login en App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import Login from './components/access/Login';
 // import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Login></Login>
     </div>
   );
 }
