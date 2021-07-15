1f995e9b5ce22ee6efd862c248590e74f0cc1a5b
CAMILOVILLABON123 <71651939+CAMILOVILLABON123@users.noreply.github.com>
2021-07-14 23:03:19 -0500

    Este Commit fue de tarjeta funcionando

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import Catalogo from './components/Catalogo';
 // import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Catalogo></Catalogo>
     </div>
   );
 }
