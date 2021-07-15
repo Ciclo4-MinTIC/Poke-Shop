c16f386c979b82fdfb6fce9973981f8323d485b3
angelica1910 <angelica.gutierrez1910@gmail.com>
2021-07-15 00:25:06 -0500

    Creación de modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import VistaProducto from './components/VistaProducto';
 // import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <VistaProducto></VistaProducto>
     </div>
   );
 }
