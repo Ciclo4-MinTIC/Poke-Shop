edfdd75ed712476aa22fdc1e59271deb95eae511
Camilost1404 <camitorres1404@gmail.com>
2021-07-14 19:26:43 -0500

    Configuración Inicial

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Link, Route, Switch } from "react-router-dom";
+import { Login } from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
