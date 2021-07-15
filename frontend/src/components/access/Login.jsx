6df58cfc8278191021c39c29b2b84dc3ea2c9297
Camilost1404 <camitorres1404@gmail.com>
2021-07-15 00:31:05 -0500

    Base del Login Creado

--- frontend/src/components/access/Login.jsx
+++ frontend/src/components/access/Login.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div>
-            
-        </div>
+        <form>
+            <div>
+                <h3>Poke-Shop</h3>
+            </div>
+            <div className="form-floating mb-3">
+                <input type="email" className="form-control" id="InputEmail" placeholder="name@example.com" required/>
+                <label for="InputEmail">Email address</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="password" className="form-control" id="InputPassword" placeholder="Password" required/>
+                <label for="InputPassword">Password</label>
+            </div>
+            <div>
+                <button className="btn btn-primary">Ingresar</button>
+            </div>            
+        </form>
     )
 }
 
