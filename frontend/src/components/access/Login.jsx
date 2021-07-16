d20376379048f6dfdf4c808ac8a0242c1fb15827
Camilost1404 <camitorres1404@gmail.com>
2021-07-15 21:31:48 -0500

    Correciónes Login

--- frontend/src/components/access/Login.jsx
+++ frontend/src/components/access/Login.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <form>
+        <div>
             <div>
                 <h3>Poke-Shop</h3>
             </div>
-            <div className="form-floating mb-3">
-                <input type="email" className="form-control" id="InputEmail" placeholder="name@example.com" required/>
-                <label for="InputEmail">Email address</label>
-            </div>
-            <div class="form-floating mb-3">
-                <input type="password" className="form-control" id="InputPassword" placeholder="Password" required/>
-                <label for="InputPassword">Password</label>
-            </div>
-            <div>
-                <button className="btn btn-primary">Ingresar</button>
-            </div>            
-        </form>
+            <form>
+                <div className="form-floating mb-3">
+                    <input type="email" className="form-control" id="InputEmail" placeholder="name@example.com" required/>
+                    <label for="InputEmail">Email address</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="password" className="form-control" id="InputPassword" placeholder="Password" required/>
+                    <label for="InputPassword">Password</label>
+                </div>
+                <div>
+                    <button className="btn btn-primary">Ingresar</button>
+                </div>            
+            </form>
+        </div>
     )
 }
 
