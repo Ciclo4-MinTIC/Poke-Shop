a42c78775e45ff9272f1b7fd599601f6326129af
wnicolas <nicolas.buitrago2gmail.com>
2021-07-15 19:17:10 -0500

    Inicio de Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 // import { Link, Route, Switch } from "react-router-dom";
+import Home from './components/Home.jsx';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home></Home>
     </div>
   );
 }
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from  './Navbar'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className="container-fluid m-0 banner">
+                <img className="" src="https://as.com/meristation/imagenes/2020/02/13/noticias/1581576624_508899_1581576871_noticia_normal.jpg" alt="Banner principal" />
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home
