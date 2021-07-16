202ceda81216e897c035cca92392843d5ce7bd99
wnicolas <nicolas.buitrago2gmail.com>
2021-07-15 19:47:13 -0500

    Avance en el home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <div className="container-fluid m-0 banner">
-                <img className="" src="https://as.com/meristation/imagenes/2020/02/13/noticias/1581576624_508899_1581576871_noticia_normal.jpg" alt="Banner principal" />
+            <div className="container-fluid m-0 p-0 banner">
+                <img src="https://as.com/meristation/imagenes/2020/02/13/noticias/1581576624_508899_1581576871_noticia_normal.jpg" alt="Banner principal" />
             </div>
             
         </div>
