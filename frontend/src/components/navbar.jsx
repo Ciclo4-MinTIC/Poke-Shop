52479e18110a773d2681cf4f2ae89abfb63f2ecf
Camilost1404 <camitorres1404@gmail.com>
2021-07-16 14:22:57 -0500

    Cambios finales

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 // Agregar este boton al navbar para abrir el modal de acceso
-//  <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+//  <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#accessModal">
 //     Acceso
 //   </button>
 
