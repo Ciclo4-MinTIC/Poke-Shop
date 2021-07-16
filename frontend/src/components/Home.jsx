2a4197be065c040155e08450f996c74e297174c6
wnicolas <nicolas.buitrago2gmail.com>
2021-07-15 19:59:14 -0500

    Avance home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <div className="container-fluid m-0 p-0 banner">
-                <img src="https://as.com/meristation/imagenes/2020/02/13/noticias/1581576624_508899_1581576871_noticia_normal.jpg" alt="Banner principal" />
-            </div>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="container-fluid m-0 p-0 banner">
+        <img
+          src="https://as.com/meristation/imagenes/2020/02/13/noticias/1581576624_508899_1581576871_noticia_normal.jpg"
+          alt="Banner principal"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
