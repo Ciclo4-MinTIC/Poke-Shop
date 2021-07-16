1d07857a7c3b4beb3b15a5dc5a8badd6b9437d83
wnicolas <nicolas.buitrago2gmail.com>
2021-07-15 19:41:16 -0500

    Avance en el Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 // import { Link, Route, Switch } from "react-router-dom";
+import Navbar from  './components/Navbar'
 import Home from './components/Home.jsx';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <Home></Home>
     </div>
   );
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import Navbar from  './Navbar'
 
 const Home = () => {
     return (
         <div>
-            <Navbar></Navbar>
             <div className="container-fluid m-0 banner">
                 <img className="" src="https://as.com/meristation/imagenes/2020/02/13/noticias/1581576624_508899_1581576871_noticia_normal.jpg" alt="Banner principal" />
             </div>
