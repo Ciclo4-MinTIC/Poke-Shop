a5783aa20a01bd4b2b0d8a2bc0c51f50c4a15a1a
Camilost1404 <camitorres1404@gmail.com>
2021-07-19 18:23:02 -0500

    Ultimos cambios al aspecto del access

--- frontend/src/components/Access.jsx
+++ frontend/src/components/Access.jsx
@@ -25,7 +25,7 @@ const Access = () => {
                     <div className="modal-content bg-dark border">
                         <div className="modal-header">
                             <h4 className="modal-title text-warning fw-bold font-monospace" id="accessModalLabel">Poke-Shop</h4>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <button type="button" class="btn-close bg-warning" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-header justify-content-center">
                             <h4 className="text-warning fw-bold font-monospace">{stateCover}</h4>
