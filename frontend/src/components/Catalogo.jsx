bb059ea457498fbdd21d668f3a56041ab68e7acc
Camilost1404 <camitorres1404@gmail.com>
2021-08-05 23:39:33 -0500

    Autenticación y cambios en el componente catalogo

--- frontend/src/components/Catalogo.jsx
+++ frontend/src/components/Catalogo.jsx
@@ -42,74 +42,74 @@ const Catalogo = ({title, imageSource, text, informacion}) => {
             aria-labelledby="exampleModalLabel"
             aria-hidden="true"
           >
-            <div className="modal-dialog modal-lg">
-              <div className="modal-content ">
-                <div className="modal-header bg-dark ">
-                  <h3 className="modal-title text-light " id="exampleModalLabel">
-                    {informacion.titulo}
-                  </h3>
-                  <button
-                    type="button"
-                    className="btn-close "
-                    data-bs-dismiss="modal"
-                    aria-label="Close "
-                  ></button>
-                </div>
-                <div className="modal-body">
-                  <div className="container-fluid">
-                    <div className="row">
-                      <div className="col-md-4 borde-gris bg-dark">                  
-                        <img
-                          src={informacion.imagen}
-                          className="img-fluid"
-                          onTouchCancel
-                          alt="imagen"
-                        ></img>                   
+              <div className="modal-dialog modal-lg">
+                <div className="modal-content ">
+                  <div className="modal-header bg-dark ">
+                    <h3 className="modal-title text-light " id="exampleModalLabel">
+                      {informacion.titulo}
+                    </h3>
+                    <button
+                      type="button"
+                      className="btn-close "
+                      data-bs-dismiss="modal"
+                      aria-label="Close "
+                    ></button>
+                  </div>
+                  <div className="modal-body">
+                    <div className="container-fluid">
+                      <div className="row">
+                        <div className="col-md-4 borde-gris bg-dark">                  
+                          <img
+                            src={informacion.imagen}
+                            className="img-fluid"
+                            onTouchCancel
+                            alt="imagen"
+                          ></img>                   
 
-                      </div>
-                      <div className="col-md-8 ">
+                        </div>
+                        <div className="col-md-8 ">
+                          
+
+                          <dl className="row">
+                            <dt className="col-sm-3 text-start text-dark">Calificación:</dt>
+                            <dd className="col-sm-9 text-start text-dark">{informacion.calificacion}</dd>
+
+                            <dt className="col-sm-3 text-start text-dark">Precio:</dt>
+                            <dd className="col-sm-9 text-start text-dark">{informacion.precio}</dd>
+
+                            <dt className="col-sm-3 text-start text-dark">Tipo:</dt>
+                            <dd className="col-sm-9 text-start text-dark">{informacion.tipo}</dd>
+
+                            <dt className="col-sm-3 text-start text-dark">Habilidad:</dt>
+                            <dd className="col-sm-9 text-start text-dark">{informacion.habilidad}</dd>
+
+                            <dt className="col-sm-3 text-start text-dark ">Altura:</dt>
+                            <dd className="col-sm-9 text-start text-dark">{informacion.altura}</dd>
+
+                            <dt className="col-sm-3 text-start text-dark">
+                                Descripción del producto:</dt>
+                            <dd className="col-sm-9 text-start text-dark">
+                            {informacion.descripcion}
+                            </dd>
+                          </dl>
+                        </div>
+
                         
-
-                        <dl className="row">
-                          <dt className="col-sm-3 text-start text-dark">Calificación:</dt>
-                          <dd className="col-sm-9 text-start text-dark">{informacion.calificacion}</dd>
-
-                          <dt className="col-sm-3 text-start text-dark">Precio:</dt>
-                          <dd className="col-sm-9 text-start text-dark">{informacion.precio}</dd>
-
-                          <dt className="col-sm-3 text-start text-dark">Tipo:</dt>
-                          <dd className="col-sm-9 text-start text-dark">{informacion.tipo}</dd>
-
-                          <dt className="col-sm-3 text-start text-dark">Habilidad:</dt>
-                          <dd className="col-sm-9 text-start text-dark">{informacion.habilidad}</dd>
-
-                          <dt className="col-sm-3 text-start text-dark ">Altura:</dt>
-                          <dd className="col-sm-9 text-start text-dark">{informacion.altura}</dd>
-
-                          <dt className="col-sm-3 text-start text-dark">
-                              Descripción del producto:</dt>
-                          <dd className="col-sm-9 text-start text-dark">
-                          {informacion.descripcion}
-                          </dd>
-                        </dl>
                       </div>
-
-                      
                     </div>
                   </div>
-                </div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-outline-warning"
-                    data-bs-dismiss="modal"
-                  >
-                    Cerrar
-                  </button>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-outline-warning"
+                      data-bs-dismiss="modal"
+                    >
+                      Cerrar
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-      </div>
+          </div>
 
           
           
